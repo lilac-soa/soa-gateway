024f7a33f32c23b044960e481298bf71ce4d966c
helloJosh <kimjoshua135@gmail.com>
2024-08-28 00:05:32 +0900

    feat: first commit

---
+++ src/test/java/com/lilac/soagateway/SoaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilac.soagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
